244bbb4cc4440b3a178b8090b45df1866d97da06
agusbw <agusbudhiarta81@gmail.com>
2023-11-24 00:58:12 +0800

    feat: add px

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 
 export default function NotFound() {
   return (
-    <div className="flex flex-col gap-2 justify-center items-center w-screen h-screen">
+    <div className="flex flex-col gap-2 justify-center text-center items-center px-8 w-screen h-screen">
       <h2 className="text-2xl font-semibold">Resource tidak ditemukan 😿</h2>
       <p>Halaman yang kamu cari gaada disini!</p>
       <Button asChild>
