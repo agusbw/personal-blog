63d63d9f953921f785cffb0e1a6b029bcfebe42d
agusbw <agusbudhiarta81@gmail.com>
2023-11-25 22:01:14 +0800

    feat: add sitemap functionality

---
+++ src/app/sitemap.ts
@@ -0,0 +1,31 @@
+import { getSortedPosts } from "../lib/server/keystatic";
+
+const URL = process.env.URL || "https://blog.agusbw.live";
+
+export default async function sitemap() {
+  const posts = await getSortedPosts();
+
+  const pages = [
+    {
+      url: `${URL}/`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 1,
+    },
+    {
+      url: `${URL}/tags`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.5,
+    },
+  ];
+
+  const postsSitemap = posts.map((post) => ({
+    url: `${URL}/posts/${post.slug}`,
+    lastModified: new Date(post.entry.updatedAt),
+    changeFrequency: "monthly",
+    priority: 0.8,
+  }));
+
+  return [...pages, ...postsSitemap];
+}
