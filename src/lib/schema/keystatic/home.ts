a6436d55f5366847b288466ace6c41b3dc663e3c
agusbw <agusbudhiarta81@gmail.com>
2023-11-26 18:54:14 +0800

    chore: move home folder

--- src/lib/schema/keystatic/home.ts
+++ src/lib/schema/keystatic/home.ts
@@ -2,6 +2,7 @@ import { singleton, fields } from "@keystatic/core";
 
 const homeSchema = singleton({
   label: "Home Page",
+  path: "/src/content/home/index",
   schema: {
     description: fields.text({
       label: "Site Description",
