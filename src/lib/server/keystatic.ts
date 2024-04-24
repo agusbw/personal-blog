b12c40ba7fd214e5ee17477840bc6ac0c9d5b547
agusbw <agusbudhiarta81@gmail.com>
2024-04-24 10:26:19 +0800

    refactor: use function chaining

--- src/lib/server/keystatic.ts
+++ src/lib/server/keystatic.ts
@@ -8,35 +8,32 @@ export const getReader = cache(() =>
   createReader(process.cwd(), keystaticConfig)
 );
 
+const reader = getReader();
+
 //collections
 export const getPost = cache(getReader().collections.posts.read);
-export const getSortedPosts = cache(async () => {
-  const reader = getReader();
-  const posts = (await reader.collections.posts.all()).filter(
-    (post) => !post.entry.draft
-  );
-  const sortedPosts = posts.sort(
-    (a, b) =>
-      new Date(b.entry.createdAt).getTime() -
-      new Date(a.entry.createdAt).getTime()
-  );
 
-  return sortedPosts;
-});
+export const getSortedPosts = cache(async () =>
+  (await reader.collections.posts.all())
+    .filter((post) => !post.entry.draft)
+    .sort(
+      (a, b) =>
+        new Date(b.entry.createdAt).getTime() -
+        new Date(a.entry.createdAt).getTime()
+    )
+);
 
 export const getFootprint = cache(getReader().collections.footprints.read);
-export const getSortedFootprints = cache(async () => {
-  const reader = getReader();
-  const footprints = (await reader.collections.footprints.all()).filter(
-    (footprint) => !footprint.entry.draft
-  );
-  const sortedFootprints = footprints.sort(
-    (a, b) =>
-      new Date(b.entry.createdAt).getTime() -
-      new Date(a.entry.createdAt).getTime()
-  );
-  return sortedFootprints;
-});
+
+export const getSortedFootprints = cache(async () =>
+  (await reader.collections.footprints.all())
+    .filter((footprint) => !footprint.entry.draft)
+    .sort(
+      (a, b) =>
+        new Date(b.entry.createdAt).getTime() -
+        new Date(a.entry.createdAt).getTime()
+    )
+);
 
 //singletons
 export const getHomePage = cache(getReader().singletons.home.read);
